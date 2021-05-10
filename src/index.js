9ab066a95958c15b5a5d111d3ac47b85596f759d
Gaurav kumar <38521462+kumargaurav05@users.noreply.github.com>
2021-05-10 05:49:28 +0530

    exercise app backend done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,106 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Sarah" />;
+
+// function formatDate(date) {
+//   return date.toLocaleDateString();
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <img className="Avatar"
+//           src={props.author.avatarUrl}
+//           alt={props.author.name}
+//         />
+//         <div className="UserInfo-name">
+//           {props.author.name}
+//         </div>
+//       </div>
+//       <div className="Comment-text">
+//         {props.text}
+//       </div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
+
+// const comment = {
+//   date: new Date(),
+//   text: 'I am very excited to learn react',
+//   author: {
+//     name: 'Gaurav',
+//     avatarUrl: 'https://placekitten.com/g/64/64',
+//   },
+// };
+
+// ReactDOM.render(<Comment
+//                   date = {comment.date}
+//                   text = {comment.text}
+//                   author = {comment.author}
+// />, document.getElementById("root"));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// function activateLasers() {
+//   alert("few few few fwe ffew few fweffwe");
+// }
+ 
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {date: new Date()};
+//   }
+
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(), 1000
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
+
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, Ji!</h1>
+//         <h2>Current time is {new Date().toLocaleTimeString()}. </h2>
+//         <button onClick= {activateLasers} >
+//           Activate Lasers
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
+// setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
